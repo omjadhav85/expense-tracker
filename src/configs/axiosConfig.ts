e4c9486934d3b2c734a0fe4f9008bc29119a02f5
Omkar Jadhav <jadhavomkar85@gmail.com>
2024-07-07 21:07:53 +0530

    [feat] added interceptor in axios request

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -1,7 +1,22 @@
+import { USER_DATA } from '@/lib/constants';
 import axios from 'axios';
 
 export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // defined inside vite.config
+});
+
+axiosClient.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  if (
+    !config.url?.includes('/api/users/login') &&
+    !config.url?.includes('/api/users/signup')
+  ) {
+    const userData = JSON.parse(localStorage.getItem(USER_DATA) || '{}');
+
+    const token = userData.token;
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosClient;
